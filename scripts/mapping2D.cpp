1c03cbc9413e33089ef4fcbca8790ddfdc42c69f
JHopeCollins <jhc.jss@gmail.com>
2019-02-11 15:49:13 +0000

    corrected some typos in quadtosquare mapping matrix calculation. Still incorrect, need to check results against pen-paper

--- scripts/mapping2D.cpp
+++ scripts/mapping2D.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
+#include <string.h>
 
 #include "quadtosquare.h"
 
    int main()
   {
+      Quad_Square_Transform<double> map;
+
+      double   x0[4]={ 0.0, 2.0, 4.0, 0.0 };
+      double   y0[4]={ 0.0, 0.0, 4.0, 2.0 };
+
+      double   p0[2]={ 0.0, 0.0 };
+      double   p1[2]={ 1.0, 0.0 };
+      double   p2[2]={ 1.0, 1.0 };
+      double   p3[2]={ 0.0, 1.0 };
+
+      double   q0[2]={ 0.0, 0.0 };
+      double   q1[2]={ 0.0, 0.0 };
+      double   q2[2]={ 0.0, 0.0 };
+      double   q3[2]={ 0.0, 0.0 };
+
+
+      int         i;
+
+
+      map.set_quad_corners( x0, y0 );
+
+      map.stdcout();
+
+      map.s2q_map( p0, q0 );
+      map.s2q_map( p1, q1 );
+      map.s2q_map( p2, q2 );
+      map.s2q_map( p3, q3 );
+
+      std::cout << "q0: " << q0[0] << "," << q0[1] << std::endl;
+      std::cout << "q1: " << q1[0] << "," << q1[1] << std::endl;
+      std::cout << "q2: " << q2[0] << "," << q2[1] << std::endl;
+      std::cout << "q3: " << q3[0] << "," << q3[1] << std::endl;
+
+      map.q2s_map( q0, p0 );
+      map.q2s_map( q1, p1 );
+      map.q2s_map( q2, p2 );
+      map.q2s_map( q3, p3 );
+
+      std::cout << "p0: " << p0[0] << "," << p0[1] << std::endl;
+      std::cout << "p1: " << p1[0] << "," << p1[1] << std::endl;
+      std::cout << "p2: " << p2[0] << "," << p2[1] << std::endl;
+      std::cout << "p3: " << p3[0] << "," << p3[1] << std::endl;
+
       return 0;
   }
--- scripts/quadtosquare.h
+++ scripts/quadtosquare.h
@@ -14,12 +14,22 @@
    template <class NumberType>
    struct Quad_Square_Transform
   {
-      NumberType     one=1;
+      const NumberType     one=1;
 
       NumberType  x[4],y[4];   // coordinates of quad corners
 
       NumberType  s2q_transform[9]; // transformation matrix from quad to square
       NumberType  q2s_transform[9]; // transformation matrix from quad to square
+
+   // constructor
+      Quad_Square_Transform()
+     {
+         memset( x, 0, 4*sizeof(NumberType) );
+         memset( y, 0, 4*sizeof(NumberType) );
+
+         memset( s2q_transform, 0, 9*sizeof(NumberType) );
+         memset( q2s_transform, 0, 9*sizeof(NumberType) );
+     }
 
       void set_quad_corners( NumberType *x0, NumberType *y0 )
      {
@@ -36,14 +46,6 @@
          NumberType  a11, a21, a31, a12, a22, a32, a13, a23, a33;
          NumberType  num, den;
 
-         num = determinant22( dx3, dy3, dx2, dy2 );
-         den = determinant22( dx1, dy1, dx2, dy2 );
-         a13 = num / den;
-
-         num = determinant22( dx1, dy1, dx3, dy3 );
-         den = determinant22( dx1, dy1, dx2, dy2 );
-         a23 = num / den;
-
          dx1 = x[1] - x[2];
          dx2 = x[3] - x[2];
          dx3 = x[0] - x[1] + x[2] - x[3];
@@ -52,31 +54,39 @@
          dy2 = y[3] - y[2];
          dy3 = y[0] - y[1] + y[2] - y[3];
 
+         num = determinant22( dx3, dy3, dx2, dy2 );
+         den = determinant22( dx1, dy1, dx2, dy2 );
+         a13 = num / den;
+
+         num = determinant22( dx1, dy1, dx3, dy3 );
+         den = determinant22( dx1, dy1, dx2, dy2 );
+         a23 = num / den;
+
+         a33 = 0;
+
          a11 = x[1] - x[0] + a13*x[1];
-         a21 = x[2] - x[1] + a23*x[3];
+         a21 = x[3] - x[0] + a23*x[3];
          a31 = x[0];
 
-         a11 = y[1] - y[0] + a13*y[1];
-         a21 = y[2] - y[1] + a23*y[3];
-         a31 = y[0];
+         a12 = y[1] - y[0] + a13*y[1];
+         a22 = y[3] - y[0] + a23*y[3];
+         a32 = y[0];
 
-         a23 = 0;
-
-         q2s_transform[0]=a11;
-         q2s_transform[1]=a21;
-         q2s_transform[2]=a31;
-         q2s_transform[3]=a12;
-         q2s_transform[4]=a22;
-         q2s_transform[5]=a32;
-         q2s_transform[6]=a13;
-         q2s_transform[7]=a23;
-         q2s_transform[8]=a33;
+         s2q_transform[0]=a11;
+         s2q_transform[1]=a21;
+         s2q_transform[2]=a31;
+         s2q_transform[3]=a12;
+         s2q_transform[4]=a22;
+         s2q_transform[5]=a32;
+         s2q_transform[6]=a13;
+         s2q_transform[7]=a23;
+         s2q_transform[8]=a33;
 
          return;
      }
 
    // calculate the perspective matrix representing the projective mapping from quadrilateral to unit square
-      void calculate_q2s(){ q2s_transform = inverse33( s2q_transform ); }
+      void calculate_q2s(){ adjoint_matrix33( s2q_transform, q2s_transform ); }
 
    // bilinear interpolation from the unit square onto a quadrilateral
       void s2q_interpolate( NumberType *xs, NumberType *xq )
@@ -103,15 +113,18 @@
      {
          NumberType  xsh[3];
          NumberType  xqh[3];
+         NumberType   At[9];
 
          xsh[0]=xs[0];
          xsh[1]=xs[1];
          xsh[2]=one;
 
-         Axb_33( s2q_transform, xsh, xqh );
+         transpose33( s2q_transform, At );
 
-         xq[0]=xqh[0];
-         xq[1]=xqh[1];
+         Axb_33( At, xsh, xqh );
+
+         xq[0]=xqh[0]/( xqh[2] + 0.000000001 );
+         xq[1]=xqh[1]/( xqh[2] + 0.000000001 );
      }
 
    // map from the quadrilateral onto the unit square using the projective mapping matrix and homogeneous coordinates
@@ -119,18 +132,38 @@
      {
          NumberType  xqh[3];
          NumberType  xsh[3];
+         NumberType   At[9];
 
          xqh[0]=xq[0];
          xqh[1]=xq[1];
          xqh[2]=one;
 
-         Axb_33( q2s_transform, xqh, xsh );
+         transpose33( q2s_transform, At );
 
-         xs[0]=xsh[0];
-         xs[1]=xsh[1];
+         Axb_33( At, xqh, xsh );
+
+         xs[0]=xsh[0]/xsh[2];
+         xs[1]=xsh[1]/xsh[2];
      }
 
-  };
+   // print transform to standard outpuf
+      void stdcout()
+     {
+         std::cout << "QST.one: " << one << std::endl;
 
+         std::cout << "QST.p0: (" << x[0] << "," << y[0] << ")" << std::endl;
+         std::cout << "QST.p1: (" << x[1] << "," << y[1] << ")" << std::endl;
+         std::cout << "QST.p2: (" << x[2] << "," << y[2] << ")" << std::endl;
+         std::cout << "QST.p3: (" << x[3] << "," << y[3] << ")" << std::endl;
+
+         std::cout << "QST.s2q: (";
+         for( int i=0; i<8; i++ ){ std::cout << s2q_transform[i] << ","; }
+         std::cout << s2q_transform[8] << ")" << std::endl;
+
+         std::cout << "QST.q2s: (";
+         for( int i=0; i<8; i++ ){ std::cout << q2s_transform[i] << ","; }
+         std::cout << q2s_transform[8] << ")" << std::endl;
+     }
+  };
 
 #endif
